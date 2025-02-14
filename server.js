f64ecb4187c55d273d039aff8b8f1421c046c619
Matt Radford <quinquex@gmail.com>
2025-02-14 08:53:45 -0800

    init mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`connected to mongodb ${mongoose.connection.name}.`);
+});
 
 // GET
 
