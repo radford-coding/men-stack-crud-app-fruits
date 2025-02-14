1091f02feb99a7f335c2e396bb54ea420f822f95
Matt Radford <quinquex@gmail.com>
2025-02-14 12:11:21 -0800

    list all echoes

--- server.js
+++ server.js
@@ -13,19 +13,37 @@ mongoose.connection.on('connected', () => {
 
 const Fruit = require('./models/fruit');
 
+app.use(express.urlencoded({ extended: false }));
+
 /*  */
 
 // GET /
-
 app.get('/', async (req, res) => { // async for database connections
     res.render('index.ejs');
 });
 
-// GET /fruits/new
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.render("fruits/index.ejs", { fruits: allFruits });
+});
 
+// GET /fruits/new
 app.get('/fruits/new', (req, res) => {
     res.render('./fruits/new.ejs');
 });
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new");
+});
+
 
 
 
