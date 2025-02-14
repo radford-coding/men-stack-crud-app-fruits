5e5493b0ee2a4d019e42030830659cdce6e96b54
Matt Radford <quinquex@gmail.com>
2025-02-14 13:26:30 -0800

    update

--- server.js
+++ server.js
@@ -54,6 +54,32 @@ app.post("/fruits", async (req, res) => {
     res.redirect("/fruits");
 });
 
+// GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/edit.ejs", {
+        fruit: foundFruit,
+    });
+});
+
+// PUT
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    console.log('here');
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
+
+
 // DELETE
 app.delete("/fruits/:fruitId", async (req, res) => {
     await Fruit.findByIdAndDelete(req.params.fruitId);
